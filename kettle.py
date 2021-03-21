5aff1c0358ad176c45f5d62b59e7fe1cba1888f9
Greg Gibbs <33558582+gregoxx@users.noreply.github.com>
2021-03-21 16:00:37 +0000

    Add files via upload

--- kettle.py
+++ kettle.py
@@ -1,5 +1,25 @@
 import numpy as np 
+import matplotlib.pyplot as plt
 
-volume = int(input("Volume: "))
+#Here we define the variables
+volume = int(input("Volume (m^3): "))
 InitialTemp = 27
+Density = int(input("Density (kg/m^3): "))
+Power = int(input("Power (W): "))
+SHC = int(input("SHC: "))
+Time = int(input("Time (seconds): "))
 
+timeList = []
+[timeList.append(i) for i in range(0, Time + 1)]
+print(timeList)
+
+TempList = []
+
+[TempList.append("{:.2f}".format(InitialTemp + (Power * timeList[j])/(Density * volume * SHC))) for j in range(0, len(timeList) - 1)]
+
+
+print(TempList)
+plt.plot(TempList)
+plt.ylabel('Temperature (°C)')
+plt.xlabel('time (seconds)')
+plt.show()
